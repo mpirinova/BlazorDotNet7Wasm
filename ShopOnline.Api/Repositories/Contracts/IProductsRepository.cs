dd1253a9bdbdc5ae840f8da5e795881c1544ab2f
DAVID\Mariya Pirinova <mpirinova@david.bg>
2023-11-08 16:03:47 +0200

    Add products repository

---
+++ ShopOnline.Api/Repositories/Contracts/IProductsRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public interface IProductsRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<IEnumerable<ProductCategory>> GetCategories();
+
+        Task<Product> GetProduct(int id);
+
+        Task<ProductCategory> GetCategory(int id);
+    }
+}
---
+++ ShopOnline.Api/Repositories/ProductsRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductsRepository : IProductsRepository
+    {
+        private readonly ShopOnlineDbContext dbContext;
+
+        public ProductsRepository(ShopOnlineDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            return await this.dbContext.ProductCategories.ToListAsync();
+
+        }
+
+        public async Task<ProductCategory> GetCategory(int id)
+        {
+            return await this.dbContext.ProductCategories.SingleOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await this.dbContext.Products
+                                .Include(p => p.ProductCategory)
+                                .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await this.dbContext.Products.Include(p => p.ProductCategory).ToListAsync();
+        }
+    }
+}
