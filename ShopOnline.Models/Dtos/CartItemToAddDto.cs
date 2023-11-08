ba6ed689f4ea7ee5b5ebfa29d0c7a94c09f193a7
DAVID\Mariya Pirinova <mpirinova@david.bg>
2023-11-08 15:55:11 +0200

    Add data transfer objects

---
+++ ShopOnline.Models/Dtos/CartItemToAddDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopOnline.Models.Dtos
+{
+    public class CartItemToAddDto
+    {
+        public int CartId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
